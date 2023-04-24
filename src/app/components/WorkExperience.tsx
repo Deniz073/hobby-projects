b39d342a49eb78456ba6a9e5a76207eaa1baed64
Deniz <denizerdem@livewallgroup.com>
2023-04-24 16:13:09 +0200

    work

--- src/app/components/WorkExperience.tsx
+++ src/app/components/WorkExperience.tsx
@@ -1,4 +1,4 @@
-
+import Image from "next/image"
 
 export default function WorkExperience() {
   return (
@@ -12,59 +12,30 @@ export default function WorkExperience() {
       <ol className="mt-6 space-y-4">
         <li className="flex gap-4">
           <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-            <img alt="" loading="lazy" width="32" height="32" decoding="async" data-nimg="1" className="h-7 w-7" style={{ color: "transparent" }} src="/_next/static/media/planetaria.ecd81ade.svg" />
+            <Image alt="livewall logo" width="28" height="28" className="h-7 w-7" src="/images/livewall.jpg" />
           </div>
           <dl className="flex flex-auto flex-wrap gap-x-2">
             <dt className="sr-only">Company</dt>
-            <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">Planetaria</dd>
+            <dd className="w-full flex text-sm font-medium text-zinc-900 dark:text-zinc-100">LiveWall</dd>
             <dt className="sr-only">Role</dt>
-            <dd className="text-xs text-zinc-500 dark:text-zinc-400">CEO</dd>
+            <dd className="text-xs text-zinc-500 dark:text-zinc-400">Test Automation Intern</dd>
             <dt className="sr-only">Date</dt>
-            <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2019 until Present">
-              <time dateTime="2019">2019</time>
+            <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2008 until 2011">
+              <time dateTime="2022-08">2022-08</time>
               <span aria-hidden="true">—</span>
-              <time dateTime="2023">Present</time>
-            </dd></dl></li>
-        <li className="flex gap-4">
-          <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-            <img alt="" loading="lazy" width="28" height="28" decoding="async" data-nimg="1" className="h-7 w-7" style={{ color: "transparent" }} src="/_next/static/media/airbnb.b4000690.svg" /></div>
-          <dl className="flex flex-auto flex-wrap gap-x-2">
-            <dt className="sr-only">Company</dt>
-            <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">Airbnb</dd>
-            <dt className="sr-only">Role</dt>
-            <dd className="text-xs text-zinc-500 dark:text-zinc-400">Product Designer</dd>
-            <dt className="sr-only">Date</dt>
-            <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2014 until 2019">
-              <time dateTime="2014">2014</time> <span aria-hidden="true">—</span>
-              <time dateTime="2019">2019</time></dd>
-          </dl></li>
-        <li className="flex gap-4"><div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-          <img alt="" loading="lazy" width="28" height="28" decoding="async" data-nimg="1" className="h-7 w-7" style={{ color: "transparent " }} src="/_next/static/media/facebook.dd9e7d48.svg" /></div>
-          <dl className="flex flex-auto flex-wrap gap-x-2">
-            <dt className="sr-only">Company</dt>
-            <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">Facebook</dd>
-            <dt className="sr-only">Role</dt>
-            <dd className="text-xs text-zinc-500 dark:text-zinc-400">iOS Software Engineer</dd>
-            <dt className="sr-only">Date</dt>
-            <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2011 until 2014">
-              <time dateTime="2011">2011</time> <span aria-hidden="true">—</span>
-              <time dateTime="2014">2014</time></dd></dl></li><li className="flex gap-4">
-          <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-            <img alt="" loading="lazy" width="28" height="28" decoding="async" data-nimg="1" className="h-7 w-7" style={{ color: "transparent" }} src="/_next/static/media/starbucks.4a5bd050.svg" /></div>
-          <dl className="flex flex-auto flex-wrap gap-x-2">
-            <dt className="sr-only">Company</dt>
-            <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">Starbucks</dd>
-            <dt className="sr-only">Role</dt><dd className="text-xs text-zinc-500 dark:text-zinc-400">Shift Supervisor</dd>
-            <dt className="sr-only">Date</dt><dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2008 until 2011">
-              <time dateTime="2008">2008</time> <span aria-hidden="true">—</span>
-              <time dateTime="2011">2011</time></dd>
-          </dl></li></ol>
+              <time dateTime="2023-01">2023-01</time>
+            </dd>
+          </dl>
+        </li>
+      </ol>
       <a className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
-       href="/deniz-erdem-cv.pdf" download={true}>
+        href="/deniz-erdem-cv.pdf" download={true}>
         Download CV
         <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50">
           <path d="M4.75 8.75 8 12.25m0 0 3.25-3.5M8 12.25v-8.5" stroke-width="1.5" stroke-linecap="round" strokeLinejoin="round">
-          </path></svg></a>
+          </path>
+        </svg>
+      </a>
     </div>
   )
 }
