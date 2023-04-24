11da01ef385c589b774d93a0c87ae7db6e6d7538
Deniz <denizerdem@livewallgroup.com>
2023-04-24 17:30:51 +0200

    contact

--- src/app/components/WorkExperience/WorkExperience.tsx
+++ src/app/components/WorkExperience/WorkExperience.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import data from "./data.json"
 
 export default function WorkExperience() {
   return (
@@ -7,26 +8,28 @@ export default function WorkExperience() {
         <svg viewBox="0 0 24 24" fill="none" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true" className="h-6 w-6 flex-none"><path d="M2.75 9.75a3 3 0 0 1 3-3h12.5a3 3 0 0 1 3 3v8.5a3 3 0 0 1-3 3H5.75a3 3 0 0 1-3-3v-8.5Z" className="fill-zinc-100 stroke-zinc-400 dark:fill-zinc-100/10 dark:stroke-zinc-500">
         </path><path d="M3 14.25h6.249c.484 0 .952-.002 1.316.319l.777.682a.996.996 0 0 0 1.316 0l.777-.682c.364-.32.832-.319 1.316-.319H21M8.75 6.5V4.75a2 2 0 0 1 2-2h2.5a2 2 0 0 1 2 2V6.5" className="stroke-zinc-400 dark:stroke-zinc-500"></path>
         </svg>
-        <span className="ml-3">Work</span>
+        <span className="ml-3">Work experience</span>
       </h2>
       <ol className="mt-6 space-y-4">
-        <li className="flex gap-4">
-          <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-            <Image alt="livewall logo" width="28" height="28" className="h-7 w-7" src="/images/livewall.jpg" />
-          </div>
-          <dl className="flex flex-auto flex-wrap gap-x-2">
-            <dt className="sr-only">Company</dt>
-            <dd className="w-full flex text-sm font-medium text-zinc-900 dark:text-zinc-100">LiveWall</dd>
-            <dt className="sr-only">Role</dt>
-            <dd className="text-xs text-zinc-500 dark:text-zinc-400">Test Automation Intern</dd>
-            <dt className="sr-only">Date</dt>
-            <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2008 until 2011">
-              <time dateTime="2022-08">2022-08</time>
-              <span aria-hidden="true">—</span>
-              <time dateTime="2023-01">2023-01</time>
-            </dd>
-          </dl>
-        </li>
+        {data.map((item, index) => (
+          <li key={index} className="flex gap-4">
+            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <Image alt="livewall logo" width="28" height="28" className="h-7 w-7" src={item.logo} />
+            </div>
+            <dl className="flex flex-auto flex-wrap gap-x-2">
+              <dt className="sr-only">Company</dt>
+              <dd className="w-full flex text-sm font-medium text-zinc-900 dark:text-zinc-100">{item.company}</dd>
+              <dt className="sr-only">Role</dt>
+              <dd className="text-xs text-zinc-500 dark:text-zinc-400">{item.role}</dd>
+              <dt className="sr-only">Date</dt>
+              <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500" aria-label="2008 until 2011">
+                <time dateTime={item.dateFrom}>{item.dateFrom}</time>
+                <span aria-hidden="true">—</span>
+                <time dateTime={item.dateTo}>{item.dateTo}</time>
+              </dd>
+            </dl>
+          </li>
+        ))}
       </ol>
       <a className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full"
         href="/deniz-erdem-cv.pdf" download={true}>
