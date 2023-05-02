2b03d7c3e5d645043ebbfc20bfe49cf1dfb50035
Deniz <denizerdem@livewallgroup.com>
2023-05-02 19:14:02 +0200

    add type

--- src/framerAnimations.ts
+++ src/framerAnimations.ts
@@ -1,4 +1,6 @@
-export const fadeIn = {
+import { HTMLMotionProps } from "framer-motion";
+
+export const fadeIn: HTMLMotionProps<any> = {
   initial: { opacity: 0 },
   whileInView: { opacity: 1 },
   transition: { delay: 0.3, duration: 0.5 },
