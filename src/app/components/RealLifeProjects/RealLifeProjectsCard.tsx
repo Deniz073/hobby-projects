aefe156377ae6c234f3329aa13310a3ef5563889
Deniz <denizerdem@livewallgroup.com>
2023-05-04 23:13:41 +0200

    nextjs 13.4 and scrollbar styling

--- src/app/components/RealLifeProjects/RealLifeProjectsCard.tsx
+++ src/app/components/RealLifeProjects/RealLifeProjectsCard.tsx
@@ -14,8 +14,8 @@ interface Props {
 
 export default function RealLifeProjectsCard({ title, description, role, date, dateDisplay, tags }: Props) {
   return (
-    <motion.article {...fadeIn} className="max-w-sm w-full lg:max-w-full lg:flex">
-      <motion.div whileHover={{ scale: 1.1 }} className="shadow-xl shadow-slate-950/10 rounded-2xl bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+    <motion.article {...fadeIn} className="inline-block mt-3 lg:flex">
+      <motion.div whileHover={{ scale: 1.04 }} className=" w-64  lg:w-[25rem] lg:h-[18rem]  overflow-hidden shadow-xl shadow-slate-950/10 rounded-2xl bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
         <div className="mb-8">
           <time dateTime={date} className="text-gray-500 text-sm mt-2">
             {dateDisplay}
@@ -33,12 +33,13 @@ export default function RealLifeProjectsCard({ title, description, role, date, d
               Deniz Erdem
             </p>
             <p className="text-gray-600">{role}</p>
-
-            {tags.map((tag, index) => (
-              <span key={index} className="inline-block mt-2 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                {tag}
-              </span>
-            ))}
+            <div className="flex flex-1 flex-col md:flex-row ">
+              {tags.map((tag, index) => (
+                <span key={index} className="inline-block mt-2  flex-wrap bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                  <p className="text-sm">{tag}</p> 
+                </span>
+              ))}
+            </div>
 
           </div>
         </div>
