7828ba6a9517b3d881222753b60945cf636d954e
Deniz <denizerdem@livewallgroup.com>
2023-05-09 19:28:49 +0200

    save messages to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,8 @@ import LandingPageHeader from "./components/LandingPageHeader/LandingPageHeader"
 import RealLifeProjects from "./components/RealLifeProjects/RealLifeProjects"
 import Testimonials from "./components/Testimonials/Testimonials"
 import WorkExperience from "./components/WorkExperience/WorkExperience"
-import { PrismaClient } from "@prisma/client"
 
 export default async function Home() {
-  const prisma = new PrismaClient()
-  console.log( await prisma.user.findMany())
 
   return (
     <>
