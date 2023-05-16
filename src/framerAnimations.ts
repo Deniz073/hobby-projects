ba68babc59e502376057edb5c7dcbf02bae4ee68
Deniz <denizerdem@livewallgroup.com>
2023-05-16 12:33:18 +0200

    viewport once

--- src/framerAnimations.ts
+++ src/framerAnimations.ts
@@ -2,6 +2,7 @@ import { HTMLMotionProps } from "framer-motion";
 
 export const fadeIn: HTMLMotionProps<any> = {
   initial: { opacity: 0 },
-  whileInView: { opacity: 1 },
-  transition: { delay: 0.3, duration: 0.5 },
+  whileInView: { opacity: 1, },
+  viewport: { once: true },
+  transition: { delay: 0.3, duration: 0.5, },
 }
