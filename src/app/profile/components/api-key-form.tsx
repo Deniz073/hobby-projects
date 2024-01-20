ece59f8b3a1ea301a0d084d576fa9a9125c45a6f
Deniz <denizerdem18@gmail.com>
2024-01-20 17:55:35 +0100

    sign out

--- src/app/profile/components/api-key-form.tsx
+++ src/app/profile/components/api-key-form.tsx
@@ -19,25 +19,17 @@ export default function ApiKeyForm({ currentApiKey }: { currentApiKey: string | 
   }
 
   return (
-    <Card className="sm:w-fit md:w-2/4">
+    <Card className="w-full sm:w-[30rem] md:w-[40rem] lg:w-[50rem]">
       <CardHeader>
         <CardTitle>Api Key</CardTitle>
         <CardDescription>Generate your api key here.</CardDescription>
       </CardHeader>
       <CardContent>
-        <div className="flex flex-col space-y-1.5 mb-3">
-          <Label htmlFor="name">Api key</Label>
-          <code className="text-sm sm:text-base inline-flex items-center justify-between space-x-4 bg-gray-800 text-white rounded-lg p-4 pl-6">
-            <span className="flex gap-4 text-left">
-              <span className="flex-1">
-                <span className="text-yellow-500">
-                  {currentApiKey ? currentApiKey : "No api key generated yet"}
-                </span>
-              </span>
-            </span>
-
+        <div className="w-full mx-auto bg-gray-900 px-4 rounded-md shadow-lg relative mb-2">
+          <div className="flex justify-between items-center py-4 rounded-t-lg">
+            <span className="text-sm font-semibold text-gray-400">.env</span>
             <svg
-              className="shrink-0 h-5 w-5 text-right transition text-gray-500 hover:text-white hover:cursor-pointer"
+              className="shrink-0 h-5 w-5 text-right transition text-gray-500 text-sm hover:text-white hover:cursor-pointer"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
               onClick={copyToClipboard}
@@ -47,7 +39,12 @@ export default function ApiKeyForm({ currentApiKey }: { currentApiKey: string | 
                 d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z">
               </path>
             </svg>
-          </code>
+          </div>
+          <pre className="text-md text-white overflow-x-auto pb-3">
+            <code id="codeBlock" className="block">
+              <span className="text-yellow-500"><span className="text-white">API_KEY=</span>{currentApiKey ? currentApiKey : "No api key generated yet"}</span>
+            </code>
+          </pre>
         </div>
         <form action={generateAPiKeyForUser}>
           <div className="grid w-full items-center gap-4">
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,6 +3,9 @@ import { redirect } from "next/navigation";
 import { authOptions } from "../AuthOptions";
 import ApiKeyForm from "./components/api-key-form";
 import { getApiKeyForUser } from "./actions";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import SignOutButton from "./components/sign-out-button";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
@@ -11,13 +14,37 @@ export default async function Page() {
     redirect("/auth/login")
   }
 
+  const { user } = session;
+
   const getApiKeyResult = await getApiKeyForUser();
 
   const apiKey = getApiKeyResult.success ? getApiKeyResult.apiKey : getApiKeyResult.message;
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center ">
+    <div className="min-h-screen flex flex-col justify-center items-center space-y-3">
       <ApiKeyForm currentApiKey={apiKey} />
+
+      <Card className="w-full sm:w-[30rem] md:w-[40rem] lg:w-[50rem]">
+        <CardHeader>
+          <CardTitle>Profile</CardTitle>
+          <CardDescription>View your profile information here.</CardDescription>
+        </CardHeader>
+        <CardContent className="p-5">
+          <div className="flex justify-between">
+            <div className="flex flex-col">
+              <Label className="text-md font-bold mb-1">Name</Label>
+              <p className="text-lg text-gray-700">{user.name}</p>
+            </div>
+            <div className="flex flex-col">
+              <Label className="text-md font-bold mb-1">Email</Label>
+              <p className="text-lg text-gray-700">{user.email}</p>
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <SignOutButton />
+        </CardFooter>
+      </Card>
     </div>
   )
 }
